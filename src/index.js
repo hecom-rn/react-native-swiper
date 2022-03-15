2ba25e2c386fca250c550e7893aedfc8ca29c399
hanhu <hanhu@hecom.cn>
2022-03-15 15:00:27 +0800

    更改scrollView 为react-native-gesture-handler中ScrollView

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import {
   Text,
   View,
   ViewPropTypes,
-  ScrollView,
   Dimensions,
   TouchableOpacity,
   Platform,
   ActivityIndicator
 } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler';
 
 /**
  * Default styles
